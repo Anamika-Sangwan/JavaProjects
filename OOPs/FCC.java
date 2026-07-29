c0741591fc06de79c95abe9c8defe5f8871de63e
Anamika-Sangwan <mnianamika@gmail.com>
2026-07-29 20:12:22 +0530

    Book Class challenge

--- OOPs/FCC.java
+++ OOPs/FCC.java
@@ -1,30 +1,30 @@
 package OOPs;
 
 public class FCC {
-    public static class Person{
-        String name;
-        int age;
-        Person(){
-            this("Default", 0);
+    public static class Book{
+        String title;
+        String author;
+        Book(){
+            this.title = "Unknown";
+            this.author = "Unknown";
         }
-        Person(String name, int age){
-            this.name = name;
-            this.age = age;
+        Book(String title){
+            this.title = title;
+            this.author = "Unknown";
         }
-        public void speak(){
-            System.out.println("Person with the name " + name + "speaks! and is aged " + age);
+        Book(String title, String author){
+            this.title = title;
+            this.author = author;
+        }
+        public void showBookInfo(){
+            System.out.println("The name of the book is " + title + " written by " + author);
         }
     }
-
     public static void main(String[] args){
-        Person a = new Person("Anamika",22);
-        Person b = new Person("Ron", 18);
-        Person c = new Person("David",35);
-        Person d = new Person();
-        a.speak();
-        b.speak();
-        c.speak();
-        d.speak();
+        Book a = new Book("Gone with the Wind", "Margaret Mitchell");
+        Book b = new Book("Crime and Punishment");
+        a.showBookInfo();
+        b.showBookInfo();
     }
 
 }
